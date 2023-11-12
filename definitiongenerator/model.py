ac771cc282c4bb2dc5aa356ad8eb22c37f37472e
marmagmono <marmagmono@gmail.com>
2023-11-12 19:01:21 +0100

    Moving things around

--- definitiongenerator/model.py
+++ definitiongenerator/model.py
@@ -1,36 +1,5 @@
 from dataclasses import field, dataclass
-from typing import Any, Iterable, Optional
-
-
-def to_snake_case(s: str):
-    def generate():
-        first_char = True
-        for c in s:
-            if c.isupper():
-                if not first_char:
-                    yield "_"
-                yield c.lower()
-            else:
-                yield c
-
-            first_char = False
-
-    return "".join(generate())
-
-
-def to_camel_case(s: str):
-    def generate(input_s: str):
-        upper_case_next = False
-        for c in input_s:
-            if c == "_":
-                upper_case_next = True
-            elif upper_case_next:
-                upper_case_next = False
-                yield c.upper()
-            else:
-                yield c
-
-    return "".join(generate(s.strip("_")))
+from typing import Any, Optional
 
 
 @dataclass
